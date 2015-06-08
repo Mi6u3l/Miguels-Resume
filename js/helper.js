48b1c4f8270f747317213bf24f4303b15ce37c0d
Miguel Braga Gomes <miguel.bgomes@gmail.com>
2015-06-08 10:46:07 +0100

    feat: Renovated entire resume

--- js/helper.js
+++ js/helper.js
@@ -1,63 +1,44 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderFName = "<h1 id='name' class='first-name'>%data%</h1>";
 var HTMLheaderLName = "<h1 id='name' class='last-name'>%data%</h1>";
-var HTMLheaderRole = '<h2>%data%</h2>';
+var HTMLheaderRole = "<h2>%data%</h2>";
+var HTMLbioPic = "<img src='%data%' class='biopic'>";
+var HTMLWelcomeMsg = "<div class='welcome-message'>%data%</div>";
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = "<li class='contact-item'><span class='glyphicon glyphicon-earphone'></span>%data%</li>";
+var HTMLemail = "<li class='contact-item'><span class='glyphicon glyphicon-envelope'></span>%data%</li>";
+var HTMLgithub = "<li class='contact-item'><span class='bt-icon icon-github'></span>%data%</li>";
+var HTMLlocation = "<li class='contact-item'><span class='glyphicon glyphicon-map-marker'></span>%data%</li>";
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLworkStart = "<div class='work-entry'></div>";
+var HTMLworkEmployer = "<span class='entry-name'>%data%</span>";
+var HTMLworkTitle = "<div class='entry-title'>%data%</div>";
+var HTMLworkDates = "<div class='entry-date'>%data%</div>";
+var HTMLworkLocation = "<a href='%data'><span class='entry-location'> - %data%</span></a>";
+var HTMLworkDescription = "<p>%data%</p>";
 
-var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLprojectStart = "<div class='project-entry col-md-4 text-center'></div>";
+var HTMLprojectTitle = "<div class='entry-title'>%data%</div>";
+var HTMLprojectDates = "<span class='entry-date'>%data%</div>";
+var HTMLprojectDescription = "<p>%data%</p>";
+var HTMLprojectImage = "<img src='%data%'>";
+var HTMLprojectDemo = "<a href='#' class='btn btn-info' role='button'>Demo</a>";
+var HTMLprojectGithub = "<a href='#' class='btn btn-info' role='button'>Github</a>";
 
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLschoolStart = "<div class='education-entry'></div>";
+var HTMLschoolName = "<span class='entry-name'><a href='#'>%data%</a></span>";
+var HTMLschoolDegree = " %data%";
+var HTMLschoolDates = "<div class='entry-date'>%data%</span>";
+var HTMLschoolLocation = "<span class='entry-location'> - %data%</div>";
+var HTMLschoolMajor = "<div><em>%data%</em></div>"
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLonlineClasses = "<h3><span class='glyphicon glyphicon-headphones'></span>Online Classes</h3>";
+var HTMLonlineTitle = "<div><em>%data%</em></div>";
+var HTMLonlineSchool = "<span class='entry-name'>%data%</span>";
+var HTMLonlineDates = "<div class='entry-date'>%data%</div>";
+var HTMLonlineURL = "<a href='#'>%data%</a>";
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em>Major: %data%<br><br></em>';
-
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
-
-var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
+var internationalizeButton = "<button>Internationalize</button>";
+var googleMap = "<div id='map'></div>";
 
 
 $(document).ready(function() {
@@ -137,8 +118,8 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.k;  // latitude from the place service
-    var lon = placeData.geometry.location.D;  // longitude from the place service
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,119 +1,47 @@
-//Bio
-var bio = {
-	"first name": "Miguel",
-	"last name": "Gomes",
-	"role": "Full-Stack Web Developer",
-	"welcome message": "I've been working the last 9 years as a .Net / SharePoint consultant, I'm now specialising in HTML, CSS and JavaScript to create beautiful web experiences.",
-	"bio pic": "",
-	"contacts": 
-		{
-			"mobile number": "000-000-0000",
-			"email": "miguel.bgomes@gmail.com",
-			"github username": "Mi6u3l",
-			"location": "Dublin, Ireland"
-		},
-	"skills": ["HTML5", "CSS3", "Javascript", "C# .net", "SharePoint"]
 
-};
-
-//Education
-var education = {
-	"schools": [
-		{
-			"name": "Dublin Business School",
-			"location": "Dublin, Ireland",
-			"degree": "Advanced Diploma",
-			"majors": ["Project Management"],
-			"dates": "June 2013",
-			"url": "http://www.dbs.ie/"
-		},
-		{
-			"name": "Universidade Lusófona",
-			"location": "Lisbon, Portugal",
-			"degree": "Bachelor",
-			"majors": ["Computer Science"],
-			"dates": "June 2006",
-			"url": "http://www.ulusofona.pt/"
-		}
-	
-	]
-	
-};
-
-
-//Work
+//Work JSON 
 var work = {
 	"jobs":[
-		{
+	   {
 			"employer": "Ergo",
 			"title": "Senior Software Developer",
 			"location": "Dublin, Ireland",
 			"dates": "2012 - Present",
-			"description": "Technical architecture analysis for web based applications; <br> Software Development (HTML5, CSS3, JavaScript, C#. net, SharePoint); <br> SharePoint Consultancy."
-		}
-	]
-};
-
-// Projects
-var projects = {
-	"projects": [
+			"description": "Technical architecture analysis for web based applications;<br>Software development team coordination;<br>Monitoring and assisting delivery of development tasks;<br>Software Development (HTML5, CSS3, JavaScript, C# .net, SQL Server, SharePoint);<br>SharePoint Consultancy."
+		},
 		{
-			"title": "Resume",
-			"date worked": "Apr, 2015",
-			"description": "Resume project for Udacity Front-end Web Development Nanodegree."
+			"employer": "Portugal Telecom",
+			"title": "Senior Software Developer",
+			"location": "Lisbon, Portugal",
+			"dates": "2011 - 2012",
+			"description": "Technical architecture analysis for web based applications;<br>Software development team coordination;<br>Monitoring and assisting delivery of development tasks;<br>Software Development (C# .net, JavaScript, SQL Server, SharePoint);<br>SharePoint Consultancy."
+		},
+		{
+			"employer": "Truewind",
+			"title": "Software Developer",
+			"location": "Lisbon, Portugal",
+			"dates": "2010 - 2011",
+			"description": "Technical architecture analysis for web based applications;<br>Software Development (C# .net, JavaScript, SQL Server, SharePoint);<br>SharePoint Consultancy."
+		},
+		{
+			"employer": "Indra",
+			"title": "Software Engineer",
+			"location": "Lisbon, Portugal",
+			"dates": "2008 - 2010",
+			"description": "Technical analysis for software bespoke developments;<br>Software Development (C# .net, JavaScript, SQL Server, SharePoint)."
+		},
+		{
+			"employer": "Deloitte",
+			"title": "Consultant",
+			"location": "Lisbon, Portugal",
+			"dates": "2006 - 2008",
+			"description": "Technical analysis for software bespoke developments;<br>Software Development (C# .net, JavaScript, SQL Server, SharePoint)."
 		}
-		
+
 	]
-
+	
 };
-
-
-
-
-//Show bio
-bio.display = function(){
-	var formattedFName = HTMLheaderFName.replace("%data%", bio["first name"].toUpperCase());
-	var formattedLName = HTMLheaderLName.replace("%data%", bio["last name"].toUpperCase());
-	var formattedRole = HTMLheaderRole.replace("%data%", bio.role.toUpperCase());
-	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile number"]);
-	var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
-	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github username"]);
-	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
-	var formattedBioPic = HTMLbioPic.replace("%data%", bio["bio pic"]);
-	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio["welcome message"]);
-
-	
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedLName);
-	$("#header").prepend(formattedFName);	
-	$("#header").append(formattedWelcomeMsg);
-	$("#topContacts ul").append(formattedMobile);
-	$("#topContacts ul").append(formattedEmail);
-	$("#topContacts ul").append(formattedGithub);
-	$("#topContacts ul").append(formattedLocation);
-
-}
-
-
-
-//Show education
-education.display = function(){
-	for (var school in education.schools){	
-		$("#education").append(HTMLschoolStart);
-		$(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[school]["dates"]))
-									.append(HTMLschoolName.replace("%data%", education.schools[school]["name"]).replace("#", education.schools[school]["url"]))
-									.append(HTMLschoolLocation.replace("%data%", education.schools[school]["location"]));
-																											
-		for(var major in education.schools[school].majors){
-			$(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[school]["majors"][major]));
-		}
-	}
-	
-	
-}
-
-
-//Show work
+// Display work json info
 work.display = function(){
 	
 	for (var job in work.jobs){
@@ -126,8 +54,66 @@ work.display = function(){
 	}
 		
 }
+// Projects JSON
+var projects = {
+	"projects": [
+		{
+			"title": "Neighborhood Map",
+			"date worked": "Dec, 2014 - Jan, 2015",
+			"description": "Neighborhood Map project for Udacity Front-end Web Development Nanodegree",
+			"github": "https://github.com/mi6u3l/neighborhood-map-seperate-model",
+			"demo": "http://Mi6u3l.github.io/neighborhood-map-seperate-model/",
+			"progress": 100
+		},
+		{
+			"title": "Arcade Game",
+			"date worked": "Nov, 2014",
+			"description": "Classic arcade game clone project for Udacity Front-end Web Development Nanodegree",
+			"github": "https://github.com/Mi6u3l/Frogger-Arcade",
+			"demo": "http://Mi6u3l.github.io/Frogger-Arcade/",
+			"progress": 100
+		},
+		{
+			"title": "Resume",
+			"date worked": "Oct, 2014",
+			"description": "Resume project for Udacity Front-end Web Development Nanodegree",
+			"github": "https://github.com/Mi6u3l/frontend-nanodegree-resume",
+			"demo": "http://Mi6u3l.github.io/frontend-nanodegree-resume/",
+			"progress": 100
+		},
+		{
+			"title": "FlowForma WebSite",
+		    "date worked": "Mar, 2014 - Jun, 2014",
+			"description": "Institutional WebSite for the FlowForma product.",
+			"demo": "http://www.flowforma.com",
+			"progress": 100
+		},
+		{
+			"title": "Ergo Managed Services",
+		    "date worked": "Mar, 2015 - May, 2015",
+			"description": "Institutional WebSite for Ergo's Managed Services department.",
+			"demo": "http://www.ergo-ids.com",
+			"progress": 100
+		},
+		{
+			"title": "FlowForma Community Support",
+			"date worked": "May, 2015 - Jun, 2015",
+			"description": "WebSite site for the FlowForma community.",
+			"demo": "http://customer.flowforma.com",
+			"progress": 70
+		},
+		{
+			"title": "Conahy Shamrocks",
+			"date worked": "Jul, 2014 - Oct, 2014",
+			"description": "Institutional WebSite for the Irish gaelic team Conahyshamrocks.",
+			"demo": "http://www.conahyshamrocks.gaa.ie",
+			"progress": 100
+		}
+		
+	]
 
-// Show projects
+};
+// Display project json info
 projects.display = function(){
 	var d3Div = new Array(projects.projects.length);
 	var progressDiv = new Array(projects.projects.length);
@@ -140,11 +126,122 @@ projects.display = function(){
 										.append(HTMLprojectTitle.replace("%data%", projects.projects[project]["title"]))
 										.append(HTMLprojectDates.replace("%data%", projects.projects[project]["date worked"]))
 										.append(HTMLprojectDescription.replace("%data%", projects.projects[project]["description"]));
+			// display demo or github json info if they exisit
+			if (projects.projects[project]["demo"] != undefined )
+				$(".project-entry:last").append(HTMLprojectDemo.replace("#", projects.projects[project]["demo"]));
+			if (projects.projects[project]["github"] != undefined )
+				$(".project-entry:last").append(HTMLprojectGithub.replace("#", projects.projects[project]["github"]));
 			
+			// call d3 progress chart function
+			projects.progressChart(d3Div, progressDiv, project, 'div' + project, projects.projects[project]["progress"]);
 
 	}
 
 }
+// project d3 progress chart function
+projects.progressChart = function(d3Div, progressDiv, project, container, value) {
+
+	d3Div[project] = d3.select(document.getElementById(container));
+
+	progressDiv[project] = radialProgress(document.getElementById(container))
+        .diameter(150)
+        .value(value)
+        .render();
+};
+
+
+//Bio JSON
+var bio = {
+	"first name": "Miguel",
+	"last name": "Gomes",
+	"role": "Full-Stack Web Developer",
+	"welcome message": "I've been working the last 9 years as a .Net / SharePoint consultant, I'm now specialising in HTML, CSS and JavaScript to create beautiful web experiences.",
+	"contacts": 
+		{
+			"mobile number": "+353 (0) 1 254 9037" ,
+			"email": "miguel.bgomes.@gmail.com",
+			"github username": "mi6u3l",
+			"location": "Dublin, Ireland"
+		},
+	"skills": ["HTML", "CSS", "JavaScript", "jQuery", "C#.net", "SharePoint", "Git"]
+
+};
+bio.display = function(){
+	var formattedFName = HTMLheaderFName.replace("%data%", bio["first name"].toUpperCase());
+	var formattedLName = HTMLheaderLName.replace("%data%", bio["last name"].toUpperCase());
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role.toUpperCase());
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile number"]);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github username"]);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
+	var formattedBioPic = HTMLbioPic.replace("%data%", bio["bio pic"]);
+	var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio["welcome message"]);
+
+	
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedLName);
+	$("#header").prepend(formattedFName);	
+	$("#header").append(formattedWelcomeMsg);
+	$("#topContacts ul").append(formattedMobile);
+	$("#topContacts ul").append(formattedEmail);
+	$("#topContacts ul").append(formattedGithub);
+	$("#topContacts ul").append(formattedLocation);
+
+}
+//Education JSON
+var education = {
+	"schools": [
+		{
+			"name": "Dublin Business School",
+			"location": "Dublin, Ireland",
+			"degree": "",
+			"majors": ["Advanced Diploma in Project Management"],
+			"dates": "June 2012",
+			"url": "http://www.dbs.ie/"
+		},
+		{
+			"name": "Universidade Lusófona",
+			"location": "Lisbon, Portugal",
+			"degree": "Bachelor",
+			"majors": ["B.S. in Computer Science"],
+			"dates": "June 2006",
+			"url": "http://www.ulusofona.pt"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-end Web Development",
+			"school": "Udacity",
+			"dates": 2015,
+			"url": "https://www.udacity.com/nanodegrees"
+		}
+	],
+};
+// Display education json info
+education.display = function(){
+	for (var school in education.schools){	
+		$("#education").append(HTMLschoolStart);
+		$(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[school]["dates"]))
+									.append(HTMLschoolName.replace("%data%", education.schools[school]["name"]).replace("#", education.schools[school]["url"]))
+									.append(HTMLschoolLocation.replace("%data%", education.schools[school]["location"]));
+																											
+		for(var major in education.schools[school].majors){
+			$(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[school]["majors"][major]));
+		}
+	}
+	$("#education").append(HTMLonlineClasses);
+	// Display online courses
+	for (var onlineCourse in education.onlineCourses){	
+		var num = education.schools.length + parseInt(onlineCourse);
+		$("#education").append(HTMLschoolStart);
+		$(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse]["dates"]))
+									.append(HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse]["school"]))
+									.append(HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse]["title"]))
+									.append(HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse]["url"]));
+	}
+}
+//Map
+$("#mapDiv").append(googleMap);
 
 // chartjs library from: http://www.chartjs.org/docs/#polar-area-chart
 var polarData = [
@@ -170,23 +267,36 @@ var polarData = [
     labelcolor: "green"
   },
   {
-    value: 8,
+    value: 5,
     color: "#FDB45C",
     highlight: "#FFC870",
-    label: "C# .net",
+    label: "jQuery",
     labelcolor: "yellow"
   },
   {
-    value: 10,
+    value: 8,
     color: "#949FB1",
     highlight: "#A8B3C5",
+    label: "C#.net",
+    labelcolor: "gray"
+  },
+  {
+    value: 10,
+    color: "#23582b",
+    highlight: "#A8B3C5",
     label: "SharePoint",
-    labelcolor: "darkgray"
+    labelcolor: "darker-green"
+  },
+  {
+    value: 4,
+    color: "#4D5360",
+    highlight: "#616774",
+    label: "Git",
+    labelcolor: "darker-gray"
   }
 
-];
-
-// Show list of skill labels
+  ];
+// Display a list of skill labels
 var skillsChartLabels = function(){
 	for (skill in polarData){
 	  var skillLabel = polarData[skill].label;
@@ -196,20 +306,22 @@ var skillsChartLabels = function(){
 	}
 }
 
-//Draw doughnut graph
+// Call functions
 window.onload = function(){
 	var ctx = document.getElementById("skills-chart").getContext("2d");
-	window.myPolarArea = new Chart(ctx).Doughnut(polarData, {
+	window.myPolarArea = new Chart(ctx).PolarArea(polarData, {
   		responsive:false
 	});
 	// Call skillsChartLabels function defined
 	skillsChartLabels();
 };
-
-
+// Call bio, work, projects and education functions
 bio.display();
 work.display();
-education.display();
 projects.display();
-$("#mapDiv").append(googleMap)
+education.display();
+
+
+
+
 
